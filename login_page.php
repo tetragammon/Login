095fe15efed0fe7696b210d90f954cc357f31553
Laurentiu Cerghedean <laurentiu.cerghedean@yahoo.com>
2018-11-22 21:09:45 +0200

    Added login_page.php
    This is the page that you actual log into

---
+++ login_page.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once('config.php');
+
+?>
+
+<div class="body">
+	<div class="jumbotron">
+
+		<h1 class="display-4">Arvato Bertelsmann</h1>
+		<p class="lead">Va rugam sa introduceti datele de logare.</p>
+
+	</div>
+
+	<form class="logare">
+		<div class="form-group row">
+			<label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="inputEmail3" placeholder="Username">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="inputPassword3" class="col-sm-2 col-form-label">Parola</label>
+			<div class="col-sm-10">
+				<input type="password" class="form-control" id="inputPassword3" placeholder="Parola">
+			</div>
+		</div>
+		<div class="col-sm-10 creare">
+			<button type="submit" class="btn btn-primary btn-lg">Login</button>
+		</div>
+	</form>
+</div>
