fcf03bcf85c49a501870f8a5215058530d2574fc
Laurentiu Cerghedean <laurentiu.cerghedean@yahoo.com>
2018-11-22 22:33:21 +0200

    Update creare_cont.php
    Added the possibility to actualy insert new users to database

--- creare_cont.php
+++ creare_cont.php
@@ -2,6 +2,28 @@
 
 include_once('config.php');
 
+if(isset($_GET['creare_cont'])) {
+
+	$nume		= $_GET['nume'];
+	$prenume	= $_GET['prenume'];
+	$username	= $_GET['username'];
+	$parola		= $_GET['parola'];
+
+	$sql = "insert into useri (nume,prenume,username,parola) values ('$nume','$prenume','$username','$parola')";
+
+}
+
+	if($conn->query($sql)) {
+
+	 echo"Inregistrare reusita";
+
+	}else {
+	
+	echo "Eroare la conectare:".$conn->error;
+	}
+
+
+
 ?>
 <div class="body">
 	<div class="jumbotron">
@@ -13,23 +35,29 @@ include_once('config.php');
 		<div class="form-group row">
 			<label for="inputEmail3" class="col-sm-2 col-form-label">Nume</label>
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputEmail3" placeholder="Nume">
+				<input type="text" name="nume" class="form-control" id="inputEmail3" placeholder="Nume">
 			</div>
 		</div>
 		<div class="form-group row">
 			<label for="inputEmail3" class="col-sm-2 col-form-label">Prenume</label>
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputEmail3" placeholder="Prenume">
+				<input type="text" name="prenume" class="form-control" id="inputEmail3" placeholder="Prenume">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
+			<div class="col-sm-10">
+				<input type="text" name="username" class="form-control" id="inputEmail3" placeholder="Username">
 			</div>
 		</div>
 		<div class="form-group row">
 			<label for="inputPassword3" class="col-sm-2 col-form-label">Parola</label>
 			<div class="col-sm-10">
-				<input type="password" class="form-control" id="inputPassword3" placeholder="Parola">
+				<input type="password" name="parola" class="form-control" id="inputPassword3" placeholder="Parola">
 			</div>
 		</div>
 		<div class="col-sm-10 creare">
-			<button type="submit" class="btn btn-primary btn-lg">Creaza cont</button>
+			<button type="submit" name="creare_cont" class="btn btn-primary btn-lg">Creaza cont</button>
 		</div>
 	</form>
 
