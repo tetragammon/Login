686d79898ccb4ff9b20fb48a2b8fbed9c1ea6eee
Laurentiu Cerghedean <laurentiu.cerghedean@yahoo.com>
2018-11-22 21:11:10 +0200

    Added creare_cont.php
    Page that allows you to create an account

---
+++ creare_cont.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once('config.php');
+
+?>
+<div class="body">
+	<div class="jumbotron">
+		<h1 class="display-4">Arvato Bertelsmann</h1>
+		 <p class="lead">Va rugam introduceti datele necesare crearii contului.</p>
+	</div>
+
+	<form class="logare">
+		<div class="form-group row">
+			<label for="inputEmail3" class="col-sm-2 col-form-label">Nume</label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="inputEmail3" placeholder="Nume">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="inputEmail3" class="col-sm-2 col-form-label">Prenume</label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="inputEmail3" placeholder="Prenume">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="inputPassword3" class="col-sm-2 col-form-label">Parola</label>
+			<div class="col-sm-10">
+				<input type="password" class="form-control" id="inputPassword3" placeholder="Parola">
+			</div>
+		</div>
+		<div class="col-sm-10 creare">
+			<button type="submit" class="btn btn-primary btn-lg">Creaza cont</button>
+		</div>
+	</form>
+
+</div>
