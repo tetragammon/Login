293daab21786939bbfffb77c9b85ddeb5bb6d732
Laurentiu Cerghedean <laurentiu.cerghedean@yahoo.com>
2018-11-22 21:08:58 +0200

    Added index.php
    Page that allows you to either login or create an account

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once('config.php');
+
+?>
+
+<div class="body">
+
+	<div class="jumbotron">
+
+		<h1 class="display-4">Arvato Bertelsmann</h1>
+		<p class="lead">Va rugam sa va logati pentru a va vizualiza rezultatele.</p>
+
+		<a href="login_page.php">
+			<button type="button" class="btn btn-primary btn-lg">Login</button>
+		</a>
+
+		<a href="creare_cont.php">
+			<button type="button" class="btn btn-primary btn-lg">Creare cont</button>
+		</a>
+
+	</div>
+
+</div>
