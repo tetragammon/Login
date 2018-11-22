01f4353679cc0a62b79a0cda050f2c952b6c8a7c
Laurentiu Cerghedean <laurentiu.cerghedean@yahoo.com>
2018-11-22 22:34:06 +0200

    Update login_page.php
    Started to create the posibility to login with an admin or regular user

--- login_page.php
+++ login_page.php
@@ -2,6 +2,15 @@
 
 include_once('config.php');
 
+if(isset($_GET['login'])) {
+
+	$username	= $_GET['username'];
+	$parola		= $_GET['parola'];
+
+	$sql_username = "select from useri username";
+	$sql_parola = "select from useri";
+}
+
 ?>
 
 <div class="body">
@@ -16,17 +25,17 @@ include_once('config.php');
 		<div class="form-group row">
 			<label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputEmail3" placeholder="Username">
+				<input type="text" name="username" class="form-control" id="inputEmail3" placeholder="Username">
 			</div>
 		</div>
 		<div class="form-group row">
 			<label for="inputPassword3" class="col-sm-2 col-form-label">Parola</label>
 			<div class="col-sm-10">
-				<input type="password" class="form-control" id="inputPassword3" placeholder="Parola">
+				<input type="password" name="parola" class="form-control" id="inputPassword3" placeholder="Parola">
 			</div>
 		</div>
 		<div class="col-sm-10 creare">
-			<button type="submit" class="btn btn-primary btn-lg">Login</button>
+			<button type="submit" name="login" class="btn btn-primary btn-lg">Login</button>
 		</div>
 	</form>
 </div>
